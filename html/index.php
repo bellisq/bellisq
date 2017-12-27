73ca211abec69ca8fc63d7d61ebe87e443a9f754
Showsay You <akizuki.c10.l65@gmail.com>
2017-12-27 10:13:04 +0900

    Implement Application

--- html/index.php
+++ html/index.php
@@ -1,6 +1,40 @@
 <?php
 
+/**
+ * Bellisq PHP Framework
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 Bellisq. All Rights Reserved.
+ * @package bellisq/bellisq
+ * @since 1.0.0
+ */
+
+use Bellisq\Bellisq\Application;
+
 require('../vendor/autoload.php');
-new PDO('mysql:host=bellisq_mysql', 'root', 'pass', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-phpinfo();
+
+(function () {
+    $server = $_SERVER;
+    $get = $_GET;
+    $post = $_POST;
+    $cookie = $_COOKIE;
+    $files = $_FILES;
+    $env = $_ENV;
+
+    $_SERVER = [];
+    $_GET = [];
+    $_POST = [];
+    $_COOKIE = [];
+    $_FILES = [];
+    $_ENV = [];
+
+    new Application(
+        $server,
+        $get,
+        $post,
+        $cookie,
+        $files,
+        $env
+    );
+})();
 
---
+++ src/Application.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Bellisq\Bellisq;
+
+use Bellisq\Bellisq\Containers\FundamentalContainer;
+use Bellisq\Frontend\RequestImmutable;
+use Bellisq\Frontend\RequestMutable;
+use Bellisq\Fundamental\Directory\LogDirectory;
+use Bellisq\Fundamental\Directory\RootDirectory;
+use Bellisq\TypeMap\Utility\ObjectContainer;
+use PDO;
+
+
+/**
+ * [Class] Application
+ *
+ * @author Showsay You <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 Bellisq. All Rights Reserved.
+ * @package bellisq/bellisq
+ * @since 1.0.0
+ */
+class Application
+{
+    private const DIR_ROOT = __DIR__ . '/..';
+    private const DIR_LOG = self::DIR_ROOT . '/log';
+
+    public function __construct(
+        array $server,
+        array $get,
+        array $post,
+        array $cookies,
+        array $files,
+        array $env)
+    {
+        // $requestMutable = new RequestMutable($get, $post, $cookies, $files, $server);
+
+        // $requestImmutable = new RequestImmutable($requestMutable);
+
+
+        $fundamentalContainer = new FundamentalContainer(new ObjectContainer(
+            new RootDirectory(self::DIR_ROOT),
+            new LogDirectory(self::DIR_LOG)
+        ));
+
+
+        // new PDO('mysql:host=bellisq_mysql', 'root', 'pass', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        // phpinfo();
+    }
+}
